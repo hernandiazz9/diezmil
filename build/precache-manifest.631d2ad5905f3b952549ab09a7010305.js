989e43446cc034124ab13e636c5bc6fd077ebd60
hernandiazz9 <62017712+hernandiazz9@users.noreply.github.com>
2020-03-10 22:24:13 +1000

    Add files via upload

---
+++ build/precache-manifest.631d2ad5905f3b952549ab09a7010305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea997772e53c379706aa7be7b10b9ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0188cd76588860d1cd6c",
+    "url": "/static/css/main.c89a8608.chunk.css"
+  },
+  {
+    "revision": "bfe702d281dc41f3703b",
+    "url": "/static/js/2.93d57e7c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.93d57e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0188cd76588860d1cd6c",
+    "url": "/static/js/main.f8cc4c3e.chunk.js"
+  },
+  {
+    "revision": "3890ca0776a6f4fcd5e2",
+    "url": "/static/js/runtime-main.ba593634.js"
+  },
+  {
+    "revision": "22b69e3f70884d8048f9904febf72b46",
+    "url": "/static/media/dice-yellow.22b69e3f.jpg"
+  }
+]);
